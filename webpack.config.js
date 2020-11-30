2da49e0a0fa9f5b88852f3d785e29f412490d4d5
OisinFoley <oisinfoley@yahoo.co.uk>
2020-11-30 21:03:15 +0100

    [Feature] - Add SCSS Support
    - Updated webpack config to support translation of scss files to css
    - Added node-sass, sass and sass-loader packages to package.json
    - Delete index.css and created styles.scss (both in src)

--- webpack.config.js
+++ webpack.config.js
@@ -33,8 +33,15 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       },
     ],
   },
